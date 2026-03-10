6cc8a4d1249d97ebb16f16d9174cdbea2f5e039d
suicad005199-crypto <suicad005199@gmail.com>
2026-03-10 10:16:47 +0800

    Update lobby.js

--- js/lobby.js
+++ js/lobby.js
@@ -1,5 +1,5 @@
-import { api } from './js/api.js';
-import { auth } from './js/auth.js';
+import { api } from './api.js';
+import { auth } from './auth.js';
 
 let allGames = [];
 let filteredGames = [];
